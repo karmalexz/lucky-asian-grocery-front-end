ca6bc087bde811aad3f6355bafa57b9ffd4e9014
Frankie Tsoi <email@example.com>
2022-02-21 16:47:37 +1100

    complete product page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,46 +1,86 @@
 import React, { Component } from 'react';
+import '../App.css';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
-const BASE_PRODUCTS_URL = 'http//localhost:3000/products';
+const BASE_PRODUCTS_URL = 'http://localhost:3000/api/products';
 
-class Products extends React.Component {
-  
+
+class Products extends React.Component{
+
   state = {
-    allProducts:[],
+    products: [],
     loading: false,
     error: null
+
   };
 
   componentDidMount(){
-    this.showProductInfo();
-
-  };
-
-  showProductInfo = async() => {
-
-    this.setState({loading: true});
-    
-    try {
-
-      const res = await axios.get(BASE_PRODUCTS_URL);
-      console.log('Products Response', res.data)
-
-    } catch(err){
-
-      console.log('Error', err)
-    }
+    this.fetchProducts();
 
   }
   
-  render() {
 
-
-    return (
-      <div>
-        Product index page
-      </div>
-    );
+  fetchProducts = async () => {
+    this.setState({loading: true});
+    try{
+      const res = await axios.get(BASE_PRODUCTS_URL)
+      ;
+      this.setState({
+        products: res.data,
+        loading: false
+      })
+      console.log('check this array', this.state.products)
+      // console.log('CHECK THIS response', res.data);
+    } catch (err){
+      console.log('Error loading AJAX', err);
+      this.setState({error: err, loading: false})
+    }
   }
+  
+  render(){
+    
+    const {loading, error, products} = this.state
+    // console.log("CHECK Render PRODUCT", this.state)
+    if (error){
+      return <p>Error loading</p>
+    }
+  
+    const productsList = products.map(p => 
+        <li key={p.id}>
+        name: {p.name} <br />
+        description: {p.description} <br />
+        price: {p.price} <br />
+        stock: {p.stock} <br/>
+        <img className="product_image" src={`http://localhost:3000/assets/${p.image}`} alt={p.name} />
+    </li>
+    )
+  
+    
+      return(
+          <div>
+          <h1>All Products</h1>
+            <ul>
+                {
+                this.state.loading
+                ?
+                <p>Loading results...</p>
+                :
+                <div className="productsIndex">
+                    
+                {productsList}
+            
+                </div>
+                }
+            </ul>
+
+
+
+          </div>
+          // <div>CHECKING IF WORKING</div>
+      );
+  };
+
 }
 
 export default Products;
