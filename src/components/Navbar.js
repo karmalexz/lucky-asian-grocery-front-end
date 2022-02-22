ec481392c1fa1cd594c0e504510a637376595442
karmalexz <49818239+karmalexz@users.noreply.github.com>
2022-02-23 10:48:17 +1100

    minor change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class NavBar extends React.Component {
   render() {
     return (
@@ -11,3 +12,25 @@ class NavBar extends React.Component {
 }
 
 export default NavBar;
+
+
+
+class Products extends React.Component{
+
+  render(){
+      const {product} = this.props;
+      return(
+          <Link className="product" to={`/url/${product.id}`}>
+              <img 
+                  className="xxxx" 
+                  src={product.image} 
+                  alt="xxx">
+              </img>
+              
+              <h3>{product.name}</h3>
+              <h4 className="price">${product.price}</h4>
+          </Link>
+      );
+  };
+
+}
