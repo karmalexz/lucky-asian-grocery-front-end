0fa389243e4ea9e4ddaf30967e5ed669ed19fd7e
karmalexz <49818239+karmalexz@users.noreply.github.com>
2022-02-23 22:06:55 +1100

    css partially added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,91 +1,103 @@
-import React, { Component } from 'react';
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
-import NavBar from './Navbar';
-import Products from './Products';
-import Categories from './Categories'
-import Login from './Login'
-import axios from 'axios';
-import MyProfile from './MyProfile'
-import ProductShow from './ProductShow';
-import Cart from './Cart';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import NavBar from "./Navbar";
+import Products from "./Products";
+import Categories from "./Categories";
+import Login from "./Login";
+import axios from "axios";
+import MyProfile from "./MyProfile";
+import ProductShow from "./ProductShow";
+import Cart from "./Cart";
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = "http://localhost:3000";
 
 class Home extends Component {
-
   //App state
   state = {
-    currentUser: {}
-  }
+    currentUser: {},
+  };
 
   //function to run on component mounting
-  componentDidMount(){
+  componentDidMount() {
     this.setCurrentUser();
   }
 
   //function to set the state to the current logged in user
   setCurrentUser = () => {
     let token = "Bearer " + localStorage.getItem("jwt");
-    axios.get(`${BASE_URL}/users/current`, {
-      headers: {
-        'Authorization': token
-      }
-    })
-    .then(res => {
-      this.setState({currentUser: res.data})
-    })
-    .catch(err => console.warn(err))
-  }
+    axios
+      .get(`${BASE_URL}/users/current`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        this.setState({ currentUser: res.data });
+      })
+      .catch((err) => console.warn(err));
+  };
 
   //function to log the user out.
   handleLogout = () => {
-    this.setState({currentUser: undefined})
+    this.setState({ currentUser: undefined });
     localStorage.removeItem("jwt");
-    axios.defaults.headers.common['Authorization'] = undefined;
-  }
-  
+    axios.defaults.headers.common["Authorization"] = undefined;
+  };
+
   render() {
     return (
       <div>
-        
         <Router>
-          <nav>
+          <nav id="navbar">
+            <div className="nav-wrapper ">
               {/* Show one of two nav bars depending on if the user is logged in */}
-                {
-                  this.state.currentUser
-                  ?
-                  (
-                    <ul>
-                      <li>Welcome {this.state.currentUser.name} | </li>
-                      <li><Link to='/my_profile'>My Profile</Link></li>
-                      <li><Link to='/products'>All of Our Products</Link></li>
-                      <li><Link to='/cart'>Cart</Link></li>
-                      <li><Link onClick={this.handleLogout} to='/'>Logout</Link></li>
-                    </ul>
-                  )
-                  :
-                  (
-                    <ul>
-                      <li><Link to='/login'>Login</Link></li>
-                    </ul>
-                  )
-                }
-            </nav>
-          <Route path="/"  component={NavBar} />
-          <Route exact path="/products" component={Products}/>
-          <Route exact path="/categories" component={Categories}/>
-          <Route exact path='/my_profile' component={()=> <MyProfile userId={this.state.currentUser.id} />}/>
-          <Route exact path="/products/:id"  component={ProductShow} />
-          <Route exact path="/cart"  component={Cart} />
+              {this.state.currentUser ? (
+                <ul id="menu">
+                  <li>Welcome {this.state.currentUser.name} | </li>
+                  <li>
+                    <Link to="/my_profile">My Profile</Link>
+                  </li>
+                  <li>
+                    <Link to="/products">All of Our Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/cart">Cart</Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleLogout} to="/">
+                      Logout
+                    </Link>
+                  </li>
+                </ul>
+              ) : (
+                <ul>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          </nav>
+          <Route path="/" component={NavBar} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/categories" component={Categories} />
+          <Route
+            exact
+            path="/my_profile"
+            component={() => <MyProfile userId={this.state.currentUser.id} />}
+          />
+          <Route exact path="/products/:id" component={ProductShow} />
+          <Route exact path="/cart" component={Cart} />
           {/* <Route exact path="/cart/add/:product_id"  component={Cart} /> */}
 
-          
-
           <Route
-            exact path='/login'
-            render={(props) => <Login setCurrentUser={this.setCurrentUser}{...props}/>}
-            />
-          </Router>
+            exact
+            path="/login"
+            render={(props) => (
+              <Login setCurrentUser={this.setCurrentUser} {...props} />
+            )}
+          />
+        </Router>
       </div>
     );
   }
